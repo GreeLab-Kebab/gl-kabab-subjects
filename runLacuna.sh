5fd8142b7863a9b2290a22b555a46d9b0aac888a
Azim Afroozeh <afroozeh3@gmail.com>
2019-10-26 18:52:18 +0200

    fix runLacuna.sh

--- runLacuna.sh
+++ runLacuna.sh
@@ -4,10 +4,10 @@ rm -r ./LacunaWebPages/*
 while IFS= read line; do
   DIR=$(dirname "${line}")
   FILENAME=$(basename "${line}")
-  node ./lacuna 100-unzipped-webpages/$DIR -e $FILENAME -d lacunaWebPages/$DIR/0 -a dynamic tajs -o 0
-  node ./lacuna 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/1 -a dynamic tajs -o 1
-  node ./lacuna 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/2 -a dynamic tajs -o 2
-  node ./lacuna 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/3 -a dynamic tajs -o 3
+  node $2 100-unzipped-webpages/$DIR -e $FILENAME -d lacunaWebPages/$DIR/0 -a dynamic tajs -o 0
+  node $2 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/1 -a dynamic tajs -o 1
+  node $2 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/2 -a dynamic tajs -o 2
+  node $2 100-unzipped-webpages/$DIR -e $FILENAME  -d lacunaWebPages/$DIR/3 -a dynamic tajs -o 3
 
 
 done < "$1"
